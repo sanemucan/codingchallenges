52ab48000019160ad0517863c7d5ebf1456b4fe6
sanemucan <sanemucan@gmail.com>
2018-05-12 21:06:39 -0400

    +union and some fix in intersection

--- union.js
+++ union.js
@@ -1,12 +1,14 @@
 /*
-  CODESMITH challenge
-  Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs. 
-  BONUS: Use reduce!
+Construct a function union that compares input arrays and returns a new array that contains all elements. 
+If there are duplicate elements, only add it once to the new array. 
+Preserve the order of the elements starting from the first element of the first input array. 
+BONUS: Use reduce!
+
 */
 
 var res = [];
 
-function intersection(...theArgs) {
+function union(...theArgs) {
    var obj = [];
    var reduced = theArgs.reduce((a,b,c) => {
        var counter = 1;
@@ -18,14 +20,7 @@ function intersection(...theArgs) {
             
        );
        return a;
-   },[])
-
-       reduced.forEach( c => {
-        if(c.count === theArgs.length){
-            obj.push({number:c.number,array:c.array, index:c.index});
-        }
-        
-     })
+   },obj)
     
      obj.sort((a,b) => {
         return a.array === b.array ? a.index - b.index : a.array - b.array;
@@ -39,7 +34,6 @@ function intersection(...theArgs) {
      return res;
 
 }
-
-
-//console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
-// should log: [5, 15]
+union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5])
+// console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
+// should log: [5, 10, 15, 88, 1, 7, 100]
